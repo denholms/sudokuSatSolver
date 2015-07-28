81e0c12a09c04e221fbdfcaec0cfef7d34094d8d
CFedderly <charlottefedderly@gmail.com>
2015-07-27 22:13:08 -0700

    Got correct clause count now

--- encoding.py
+++ encoding.py
@@ -5,7 +5,7 @@ import sys, getopt, math
 
 N = 0
 C = 0
-clause_count = 0
+
 solution = []
 puzzle = []
 
@@ -22,8 +22,13 @@ def read (file):
 def write ():
 	f = open("mid.txt", "wb")
 	global solution
-	global clause_count
-	f.write("p cnf " + str(getNumberOfVariables(N-1, N-1, N-1) + 1) + " " + str(clause_count) + "\n")
+	# count total number of clauses
+	count = 0
+	for cell in solution:
+		count += 1
+
+	f.write("p cnf " + str(getNumberOfVariables(N-1, N-1, N-1) + 1) + " " + str(count) + "\n")
+	count = 0
 	for cell in solution:
 		for i in cell:
 			f.write(str(i) + " ")
@@ -50,40 +55,31 @@ def getNumberOfVariables(value, row, column):
 
 def eachCell():
 	global solution
-	global clause_count
 	for row in range (N):
 		for column in range (N):
 			clause = [];
 			for value in range (1, N+1):
 				clause.append(getNumberOfVariables(value, row, column))
 			solution.append(clause)
-			clause_count += 1
-
 
 def oncePerColumn():
 	global solution
-	global clause_count
 	for col in range (N):
 		for value in range (1, N+1):
 			for row in range (N-1):
 				for nrow in range(row+1, N):
 					solution.append([-getNumberOfVariables(value,row,col), -getNumberOfVariables(value, nrow, col)])
-					clause_count += 1
 
 def oncePerRow():
 	global solution
-	global clause_count
 	for row in range (N):
 		for value in range (1, N+1):
 			for column in range (N-1):
 				for ncol in range(column+1, N):
 					solution.append([-getNumberOfVariables(value, row, column), -getNumberOfVariables(value, row, ncol)])
-					clause_count += 1
-
 
 def oncePerGrid():
 	global solution
-	global clause_count
 	for value in range (1, N+1):
 		for xbox in range(C):
 			for ybox in range(C):
@@ -92,57 +88,43 @@ def oncePerGrid():
 						for k in range(i+1, C):
 							for l in range(C):
 								solution.append([-getNumberOfVariables(value, (C * xbox) + i, (C * ybox) + j), -getNumberOfVariables(value, (C * xbox) + k, (C * ybox) + l)])
-				        clause_count += 1
 
 def addFromPuzzle():
 	global puzzle
 	global solution
-	global clause_count
 	for row in range(N):
 		for column in range(N):
 			if(puzzle[row][column] > 0):
 				solution.append([getNumberOfVariables(puzzle[row][column], row, column)])
-				clause_count += 1
-
 
 #one number per cell
 def checkCells():
-	global clause_count
 	for row in range(N):
 		for column in range(N):
 			for i in range(1, N):
 				for j in range(i+1, N+1):
 					solution.append([-getNumberOfVariables(i, row, column), -getNumberOfVariables(j, row, column)])
-					clause_count += 1
-
-
 
 #one number per row
 def checkRows():
-	global clause_count
 	for i in range(1, N+1):
 		for column in range(N):
 			vals = []
 			for row in range(N):
 				vals.append(getNumberOfVariables(i, row, column))
 			solution.append(vals);
-			clause_count += 1
 
 #one number per col
 def checkCols():
-	global clause_count
 	for i in range(1, N+1):
 		for row in range(N):
 			vals = []
 			for column in range(N):
 				vals.append(getNumberOfVariables(i, row, column))
 			solution.append(vals);
-			clause_count += 1
-
 
 #one number per grid
 def checkGrids():
-	global clause_count
 	for gridRow in range(C):
 		for gridColumn in range(C):
 			for i in range(1, N+1):
@@ -151,8 +133,6 @@ def checkGrids():
 					for k in range(C):
 						vals.append(getNumberOfVariables(i, j + (C * gridRow), k + (C * gridColumn)))
 				solution.append(vals)
-				clause_count += 1
-
 
 def extendedEncoding():
 	checkCells()
